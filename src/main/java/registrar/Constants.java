abad978f05097661d02ea23d0d4b222b733a6b88
akatorets <a.katorets@gmail.com>
2018-01-21 21:44:27 +0300

    update app to get constants from properties

--- src/main/java/registrar/Constants.java
+++ src/main/java/registrar/Constants.java
@@ -1,12 +1,12 @@
 package registrar;
 
 public class Constants {
-    public static final Integer USER_ID = null;
-    public static final String LOGIN = null;
-    public static final String PASSWORD = null;
-    public static final String APPLICATION_ID = null;
-    public static final String SCOPE = null;
-    public static final String GROUP_ID = null;
+    public static final Integer USER_ID = Integer.parseInt(System.getProperty("user"));
+    public static final String LOGIN = System.getProperty("login");
+    public static final String PASSWORD = System.getProperty("password");
+    public static final String APPLICATION_ID = System.getProperty("application");
+    public static final String SCOPE = System.getProperty("scope");
+    public static final String GROUP_ID = System.getProperty("group");
 
     private Constants() {}
 
